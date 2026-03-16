4df6686c3d1d093f89fd6055aca1f734253ce4f4
Allamoment <allamoment@gmail.com>
2026-03-17 05:01:55 +0900

    add pagination, filtering and validation

--- src/server.js
+++ src/server.js
@@ -1,5 +1,33 @@
-// src/index.js
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import { errors } from 'celebrate';
+import { connectMongoDB } from './db/connectMongoDB.js';
+import { logger } from './middleware/logger.js';
+import { notFoundHandler } from './middleware/notFoundHandler.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import notesRoutes from './routes/notesRoutes.js';
 
-const message = 'Hello world';
+const PORT = process.env.PORT ?? 3000;
 
-console.log(message);
+const startServer = async () => {
+  await connectMongoDB();
+
+  const app = express();
+
+  app.use(logger);
+  app.use(cors());
+  app.use(express.json());
+
+  app.use(notesRoutes);
+
+  app.use(errors());
+  app.use(notFoundHandler);
+  app.use(errorHandler);
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+startServer();
