ca7c394712afc6f06a4dc79f0c62cce3a9f1d3b0
Allamoment <allamoment@gmail.com>
2026-03-18 04:58:40 +0900

    fix validation schemas

--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -1,17 +1,17 @@
-import { celebrate, Joi, Segments } from 'celebrate';
+import { Joi, Segments } from 'celebrate';
 import { isValidObjectId } from 'mongoose';
 import { TAGS } from '../constants/tags.js';
 
-export const getAllNotesSchema = celebrate({
+export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
     perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
     search: Joi.string().allow(''),
   }),
-});
+};
 
-export const noteIdSchema = celebrate({
+export const noteIdSchema = {
   [Segments.PARAMS]: Joi.object({
     noteId: Joi.string().custom((value, helpers) => {
       if (!isValidObjectId(value)) {
@@ -20,17 +20,17 @@ export const noteIdSchema = celebrate({
       return value;
     }),
   }),
-});
+};
 
-export const createNoteSchema = celebrate({
+export const createNoteSchema = {
   [Segments.BODY]: Joi.object({
     title: Joi.string().min(1).required(),
     content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
   }),
-});
+};
 
-export const updateNoteSchema = celebrate({
+export const updateNoteSchema = {
   [Segments.PARAMS]: Joi.object({
     noteId: Joi.string().custom((value, helpers) => {
       if (!isValidObjectId(value)) {
@@ -44,4 +44,4 @@ export const updateNoteSchema = celebrate({
     content: Joi.string().allow(''),
     tag: Joi.string().valid(...TAGS),
   }).min(1),
-});
+};
