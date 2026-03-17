ec92e4b9c975d5acfbef2f31284c0115c6eb40fa
Allamoment <allamoment@gmail.com>
2026-03-18 04:47:48 +0900

    add auth

--- src/models/note.js
+++ src/models/note.js
@@ -3,6 +3,7 @@ import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
+    userId: { type: Schema.Types.ObjectId, required: true, ref: 'User' },
     title: { type: String, required: true, trim: true },
     content: { type: String, default: '', trim: true },
     tag: { type: String, enum: TAGS, default: 'Todo' },
